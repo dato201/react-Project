60ae29e5937164712939a2915a55e209fc673608
dato201 <146935809+dato201@users.noreply.github.com>
2024-02-26 18:46:42 +0400

    add react project dan

--- src/molecules/SignUpBar/SignUpBar.jsx
+++ src/molecules/SignUpBar/SignUpBar.jsx
@@ -1,12 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import classes from './SignUpBar.module.css'
+import { useTranslation } from 'react-i18next';
 
 const SignUpBar = () => {
+  const { t, i18n } = useTranslation();
 
     return (
         <div className={classes.Sign_div}>
-            <NavLink to={'/sign-up'}><button className={classes.button}>Sign Up</button></NavLink>
-            <button className={classes.button}>Log In</button>
+            <NavLink to={'/sign-up'}><button className={classes.button}>{t('Sign Up')}</button></NavLink>
+            <button className={classes.button}>{t('Log In')}</button>
         </div>
     );
 }
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,7 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import classes from "./SignUpPage.module.css";
 import { useFormik } from "formik";
 import * as yup from 'yup';
+import { classNames } from "primereact/utils";
+import { $api, TOKEN_LOCAL_STORAGE_KEY } from "../../atoms/config/api";
+import { useTranslation } from "react-i18next";
 
 
 const validationSchema = yup.object().shape({
@@ -12,6 +15,9 @@ const validationSchema = yup.object().shape({
 });
 
 const SignUpPage = () => {
+  const { t, i18n } = useTranslation();
+
+  const navigation = useNavigate();
 
   const formik = useFormik({
     validationSchema: validationSchema,
@@ -21,16 +27,24 @@ const SignUpPage = () => {
       email: "",
       password: ""
     },
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: async (values) => {
+      const response = await $api.post('/auth', {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        email: values.email,
+        password: values.password
+      })
+
+      localStorage.setItem('user', JSON.stringify(values));
+      localStorage.setItem(TOKEN_LOCAL_STORAGE_KEY, response.data.token);
+
+      if (response.status === 200 ) {
+        navigation('/');
+      }
     }
   })
 
   const isFormFieldInvalid = (name) => !!(formik.touched[name] && formik.errors[name]);
-
-  if (formik.touched.password && formik.errors.password) {
-    console.log("error");
-  }
 
   const submit = () => {
     formik.handleSubmit();
@@ -40,12 +54,26 @@ const SignUpPage = () => {
     <div className={classes.SignUpPage}>
       <div className={classes.panel}>
         <NavLink to={'/'}><i class="fa-solid fa-caret-left"></i>home</NavLink>
-        <h1 className={classes.h1}>Sign Up</h1>
-        <input name="firstName" value={formik.values.firstName} onChange={formik.handleChange} placeholder="First Name..." />
-        <input name="lastName" value={formik.values.lastName} onChange={formik.handleChange} placeholder="Last Name..." />
-        <input name="email" value={formik.email} onChange={formik.handleChange} type="email" placeholder="Email" />
-        <input name="password" value={formik.password} onChange={formik.handleChange} type="password" placeholder="Password" />
+        <h1 className={classes.h1}>{t('Sign Up')}</h1>
+        <input name="firstName" value={formik.values.firstName} className={classNames({'p-invalid': isFormFieldInvalid('firstName')})} onChange={formik.handleChange} placeholder="First Name..." />
+        {isFormFieldInvalid('firstName') && (
+          <span className={classes.error}>{formik.errors.firstName}</span>
+        )}        
+        <input name="lastName" value={formik.values.lastName} className={classNames({'p-invalid': isFormFieldInvalid('lastName')})} onChange={formik.handleChange} placeholder="Last Name..." />
+        {isFormFieldInvalid('lastName') && (
+          <span className={classes.error}>{formik.errors.lastName}</span>
+        )}
 
+        <input name="email" value={formik.email} className={classNames({'p-invalid': isFormFieldInvalid('email')})} onChange={formik.handleChange} type="email" placeholder="Email" />
+        {isFormFieldInvalid('email') && (
+          <span className={classes.error}>{formik.errors.email}</span>
+        )}
+        
+        <input name="password" value={formik.password} className={classNames({'p-invalid': isFormFieldInvalid('password')})} onChange={formik.handleChange} type="password" placeholder="Password" />
+
+        {isFormFieldInvalid('password') && (
+          <span className={classes.error}>{formik.errors.password}</span>
+        )}
         <button onClick={submit}>Register</button>
       </div>
     </div>
