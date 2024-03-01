d90466afa90dbe72b2fd9feacc13eab663fdeb5a
dato201 <146935809+dato201@users.noreply.github.com>
2024-03-01 20:36:51 +0400

    add react project

---
+++ src/molecules/ProductCard/ProductCard.jsx
@@ -0,0 +1,37 @@
+import classes from "./ProductCard.module.css";
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import PropTypes from "prop-types";
+
+
+const ProductCard = (props) => {
+  const { product } = props;
+
+  const header = (
+    <img className={classes.img} alt="Card" src={product.images[0]} />
+  );
+
+
+  const footer = (
+    <>
+      <Button label="Save" icon="pi pi-check" />
+      <Button label="Cancel" severity="secondary" icon="pi pi-times" style={{ marginLeft: '0.5em' }} />
+    </>
+  );
+
+  return (
+    <div className={classes.ProductCard}>
+      <Card title={product.title} subTitle={`${product.price} ₾`} footer={footer} header={header} className="md:w-25rem" >
+        <p className="m-0">
+          {product.description}
+        </p>
+      </Card>
+    </div>
+  );
+};
+
+ProductCard.propTypes = {
+  product: PropTypes.object,
+}
+
+export { ProductCard };
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,22 +1,57 @@
+import { useDispatch, useSelector } from "react-redux";
 import { $api } from "../../atoms/config/api";
 import classes from "./MainPage.module.css"
 import { useEffect } from "react";
+import { getProducts, getProductsLoading } from "../../atoms/store/model/selectors/product/productSelectors";
+import { getProductsService } from "../../atoms/store/model/services/products/getProductsService";
+import { ProductCard } from "../../molecules/ProductCard/ProductCard";
+import { Skeleton } from 'primereact/skeleton';
+// import { Layout } from "../../templates/Layout/Layout";
+
+
 
 const MainPage = (props) => {
 
   const fetchSomething = async () => {
+    const dispatch = useDispatch();
+    const products = useSelector(getProducts);
+    const productsLoading = useSelector(getProductsLoading);
+
+    useEffect(() => {
+      dispatch(getProductsService());
+    }, [dispatch])
+
     const response = await $api.get('/profile');
     console.log(response.data);
-  }
 
-  useEffect(() => {
-    fetchSomething()
-  }, [])
-  return (
-    <div className={classes.MainPage}>
-      <h1>Main Page</h1>
-    </div>
-  );
-};
+    useEffect(() => {
+      fetchSomething()
+    }, [])
+    return (
+      <div className={classes.MainPage}>
+        <Layout>
+          <h1>Products</h1>
+          <div className={classes.products}>
+            {products?.map((product) => {
+              return (
+                <ProductCard key={product.id} product={product} />
+              )
+            })}
+            {productsLoading && (
+              <>
+                <Skeleton width="300px" height="566px" />
+                <Skeleton width="300px" height="566px" />
+                <Skeleton width="300px" height="566px" />
+                <Skeleton width="300px" height="566px" />
+                <Skeleton width="300px" height="566px" />
+                <Skeleton width="300px" height="566px" />
+              </>
+            )}
+          </div>
+        </Layout>
+      </div>
+    );
+  };
+}
 
 export { MainPage };
